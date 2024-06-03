542f0116c6d045cfbb670e6de078d61bea7b4aaf
Hongbin.Rao <862396608@qq.com>
2024-06-03 09:24:28 +0800

    更新配置文件

--- server/src/test-user/dto/update-test-user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTestUserDto } from './create-test-user.dto';
-
-export class UpdateTestUserDto extends PartialType(CreateTestUserDto) {}
